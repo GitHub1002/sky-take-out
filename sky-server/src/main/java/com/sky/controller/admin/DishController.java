af0444b880bc8c22f8c8e16f0f09dd054f045e9b
GitHub1002 <2508812594@qq.com>
2024-04-04 19:04:05 +0800

    菜品分页查询

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -7,6 +7,8 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
@@ -14,10 +16,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -28,10 +27,31 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+    /**
+     * @author Mingkai Feng
+     * @date 2024/4/4 18:39
+     * @Description ToDo 新增菜品
+     * @param dishDTO
+     * @return Result
+     */
     @ApiOperation("新增菜品")
     @PostMapping
     public Result addDishWithFlavor(@RequestBody DishDTO dishDTO) {
         dishService.addDishWithFlavor(dishDTO);
         return Result.success();
+    }
+
+    /**
+     * @author Mingkai Feng
+     * @date 2024/4/4 18:39
+     * @Description ToDo 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return Result<PageResult>
+     */
+    @GetMapping("/page")
+    @ApiOperation("菜品分页查询")
+    public Result<PageResult> page (DishPageQueryDTO dishPageQueryDTO){
+        PageResult pageResult = dishService.pageQuery (dishPageQueryDTO);
+        return Result.success(pageResult);
     }
 }
--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,10 +1,13 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
 import com.sky.result.Result;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -29,4 +32,12 @@ public interface DishMapper {
     @AutoFill(value = OperationType.INSERT)
     void insert(Dish dish);
 
+    /**
+     * @author Mingkai Feng
+     * @date 2024/4/4 18:53
+     * @Description ToDo 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return Page<DishVO>
+     */
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
